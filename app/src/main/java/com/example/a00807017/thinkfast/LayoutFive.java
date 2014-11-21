c75758db941cfd6fd7353d9ad8c93cc98dddf668
sabusa <sabusa@shaw.ca>
2014-11-21 15:27:15 -0800

    Lynn's instructions now included

---
+++ app/src/main/java/com/example/a00807017/thinkfast/LayoutFive.java
@@ -0,0 +1,28 @@
+package com.example.a00807017.thinkfast;
+
+/**
+ * Created by A00807017 on 11/6/2014.
+ */
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LayoutFive extends Fragment {
+
+    public static Fragment newInstance(Context context) {
+        LayoutFive f = new LayoutFive();
+
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_five, null);
+        return root;
+    }
+
+}
---
+++ app/src/main/java/com/example/a00807017/thinkfast/LayoutTwo.java
@@ -0,0 +1,27 @@
+package com.example.a00807017.thinkfast;
+
+/**
+ * Created by A00807017 on 11/6/2014.
+ */
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LayoutTwo extends Fragment {
+
+    public static Fragment newInstance(Context context) {
+        LayoutTwo f = new LayoutTwo();
+
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.layout_two, null);
+        return root;
+    }
+
+}
